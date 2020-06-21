18a263d90a42238a46e71b1181cf94545b1c7667
jgourassas1948 <jgourassas@gmail.com>
2020-06-21 09:28:55 +0300

    still build json for graphics

--- assets/js/external_injuries/draw_external.js
+++ assets/js/external_injuries/draw_external.js
@@ -16,68 +16,139 @@ let template=[];
 
 let injury_template = [];
 
-const injury_template_2 = [
+const parent_child_template = [
   {
-    term_level: "0",
+    term_level: "1",
     data:   "",
-    parent: null
-}, 
-
-{
-  term_level: "1",
-  data:   "",
-  parent: "0"
-}, 
+    parent: null,
+    children: "2"},
 
 {
   term_level: "2",
   data:   "",
-  parent: "1"
-}, 
-
-{
-  term_level: "3",
-  data:   "",
-  parent: "2"
-}, 
-
-{
-  term_level: "4",
-  data:   "",
-  parent: "3"
-}, 
-
-{
-  term_level: "5",
-  data:   "",
-  parent: "4"
+  parent: "1",
+  children: "3"
 },
+  
+ {   
+   term_level: "3",
+    data:   "",
+    parent: "2",
+    children: "4"
+  },
+  {   
+    term_level: "4",
+     data:   "",
+     parent: "3",
+     children: "5"
+   },
+   {   
+    term_level: "5",
+     data:   "",
+     parent: "4",
+     children: "6"
+   },
+   {   
+    term_level: "6",
+     data:   "",
+     parent: "5",
+     children: "7"
+   }, 
 
-{
-  term_level: "6",
-data:   "",
-parent: "5"
+   {   
+    term_level: "7",
+     data:   "",
+     parent: "6",
+     children: "8"
+   }, 
+   {   
+    term_level: "8",
+     data:   "",
+     parent: "7",
+     children: "9"
+   }, 
+   {   
+    term_level: "9",
+     data:   "",
+     parent: "8",
+     children: null
+   }, 
+  ]
+//////////////////
+const nested_template = [
+  {
+    term_level: "0",
+    data:   "",
+    parent: null,
+    children: [
+      {
+        term_level: "1",
+        data:   "",
+        parent: "0",
+        children:[
+          {
+            term_level: "2",
+            data: "",
+            parent: "1",
+            children: [
+              {
+                term_level: "3",
+                data: "",
+                parent: "2",
+                children: [
+                  {
+                    term_level: "4",
+                data: "",
+                parent: "3",
+                children: [
+                  {
+                    term_level: "5",
+                    data: "",
+                    parent: "4",
+                    children: [
+                      {
+                        term_level: "6",
+                        data: "",
+                        parent: "5",
+                        chidren: [
+                          {
+                            term_level: "7",
+                        data: "",
+                        parent: "6",
+                        children: [
+                          {
+                            term_level: "8",
+                            data: "",
+                            parent: "7",
+                            children: [
+                              {
+                                term_level: "9",
+                            data: "",
+                            parent: "8",
+                              }
+                            ]  
+                          }
+                        ]
+                          }
+                        ]
+
+                      }
+
+                    
+                    ]
+                  }
+                ]
+              }
+                ]
+              }
+            ]
+          }
+        ] 
+      }
+          
+    ]
 },
-
-{
-  term_level: "7",
-data:   "",
-parent: "6"
-},
-
-{
-  term_level: "8",
-data:   "",
-parent: "7"
-},
-
-{
-  term_level: "9",
-data:   "",
-parent: "8"
-},
-
-
+ 
 
 ];//injury_template
 
@@ -111,7 +182,9 @@ let nested_data = []
 
 let links = [];
 
+//let color  = d3.schemeCategory20
 
+let color = d3.scaleOrdinal(d3.schemeCategory10);
 
 const get_links = () => {
   return links;
@@ -120,6 +193,14 @@ const get_links = () => {
 const get_nested_data = () => {
     return nested_data;
 }
+let unit_size = 5.5;
+
+let digits_text_path = d3.arc()
+    .startAngle(-2.2)
+    .endAngle(function(d) { return 3.5; })
+		.innerRadius(function(d) { return unit_size*12; })
+    .outerRadius(function(d) { return unit_size*16; }); 
+
 
 let margin = {
     top: 30,
@@ -130,6 +211,7 @@ let margin = {
  let width = 1100 - margin.left - margin.right;
  let  height = 1300 - margin.top - margin.bottom;
 
+ let barWidth = width  * 0.8
 
 class Guides {
     constructor() {
@@ -271,9 +353,330 @@ constructor(
 
 };//constructor
 ///////////////////////
+diagram_1(group) {
+  let tree_origin_x = width * 0.01;
+  let tree_origin_y = height * 0.09;
+ 
 
-///////////////////////
+// declares a tree layout and assigns the size
+
+let tree = d3.tree()
+.size([width, height])
+
+
+ let nodes = d3.hierarchy(nested_data);   
+    nodes = tree(nodes)
+   
+
+   
+
+//let links = tree.links(nested_data);
+
+
+  let  svg = group.append("svg")
+   .append("g")                     
+   .attr("id", "injury_tree")
+   .selectAll("g")
+   .data(nodes.descendants())
+      .enter()
+   .append("g")
+   .attr("class", "node")
+   .attr('transform', `translate(${tree_origin_x}, ${tree_origin_y})`);
+   
+   let link = svg.selectAll(".link")
+    .data( nodes.descendants().slice(1))
+  .enter().append("path")
+    .attr("class", "link")
+    .attr("d", function(d) {
+       return "M" + d.x + "," + d.y
+         + "C" + d.x + "," + (d.y + d.parent.y) / 2
+         + " " + d.parent.x + "," + (d.y + d.parent.y) / 2
+         + " " + d.parent.x + "," + d.parent.y;
+        });
+        
+  // adds each node as a group
+let  node = svg.selectAll(".node")
+.data(nodes.descendants())
+.enter().append("g")
+.attr("class", function(d) {
+  return "node" +
+    (d.children ? " node--internal" : " node--leaf"); })
+//.attr("transform", function(d) {
+//  return "translate(" + d.x + "," + d.y + ")"; });
+  .attr('transform', `translate(${width*0.06}, ${tree_origin_y})`)
+
+  
+  // adds the circle to the node
+node.append("circle")
+  .attr("r", 10)
+  .append('title')
+  .text((d,i) => {
+    return ' Title:  ' 
+    + d.term_title
+    + " Code: " 
+    + d.term_code
+    + " "
+    + " Level " 
+    + d.term_level
+       })
+  node.append("text")
+  .attr("dy", ".35em")
+  .attr("y", function(d) { return d.children ? -20 : 20; })
+  .style("text-anchor", "middle")
+  .text(function(d) {
+   console.log("DATA jgour " + d )  
+    return d.data.term_title;
+   });
+
+
+////////////////////
+};//diagram
+
+//////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
 diagram(group){
+
+  let tree_origin_x = width * 0.01;
+  let tree_origin_y = height * 0.1;
+  
+  let  svg = group.append("svg")
+  .attr("width", width)
+  .attr("height", height )
+  .append("g")
+  .attr('transform', `translate(${tree_origin_x}, ${tree_origin_y})`);
+
+// declares a tree layout and assigns the size
+let treemap = d3.tree()
+.size([width, height* 0.8])
+
+/////////////////////
+// assigns the data to a hierarchy using parent-child relationships
+//let nodes = d3.hierarchy(nested_data);  
+let nodes = d3.hierarchy(treeData);  
+// maps the node data to the tree layout
+    nodes = treemap(nodes)
+  
+   // adds each node as a group
+let  node = svg.selectAll(".node")
+.data(nodes.descendants())
+.enter().append("g")
+.attr("class", function(d) {
+  return "node" +
+    (d.children ? " node--internal" : " node--leaf"); })
+.attr("transform", function(d) {
+  return "translate(" + d.x + "," + d.y + ")"; });
+
+  node.append("circle")
+  .attr("r", 10);
+  
+  let link = svg.selectAll(".link")
+  .data( nodes.descendants().slice(1))
+.enter().append("path")
+  .attr("class", "link")
+  .attr("d", function(d) {
+     return "M" + d.x + "," + d.y
+       + "C" + d.x + "," + (d.y + d.parent.y) / 2
+       + " " + d.parent.x + "," + (d.y + d.parent.y) / 2
+       + " " + d.parent.x + "," + d.parent.y;
+     });
+
+
+     node.append("text")
+     .attr("dy", ".35em")
+     .attr("y", function(d) { return d.children ? -20 : 20; })
+     .style("text-anchor", "middle")
+     .text(function(d) { return d.data.name; });
+   
+
+///////////////////
+};//diagram
+
+
+
+
+
+
+//////////////////////
+
+
+diagram_22(group){
+  let tree_origin_x = width * 0.01;
+  let tree_origin_y = height * 0.04;
+  
+  let  svg = group.append("svg")
+  .attr("width", width)
+  .attr("height", height )
+  .append("g")
+  .attr('transform', `translate(${tree_origin_x}, ${tree_origin_y})`);
+
+// declares a tree layout and assigns the size
+let treemap = d3.tree()
+.size([width, height])
+
+/////////////////////
+// assigns the data to a hierarchy using parent-child relationships
+let nodes = d3.hierarchy(nested_data);  
+//let nodes = d3.hierarchy(treeData);  
+// maps the node data to the tree layout
+    nodes = treemap(nodes)
+/////////////////////////
+
+
+
+let  groups = svg.selectAll(".circle")
+   .data(nested_data)
+    .enter().append("g")
+  .attr("class", "circle")
+  .append("g")
+  .attr('transform', `translate(${width*0.06}, ${tree_origin_y})`);
+
+ 
+  
+
+ 
+  let circles = groups.selectAll("circle") // start a nested selection
+  .data( (d, i) => {
+    return d.values; // tell d3 where the children are
+  })
+  .enter().append("circle")
+  .style("fill", d => color(d.term_level))
+  .attr("stroke", (d) =>  {
+    return "cyan"
+      })
+  .attr('stroke-width', d => {
+    return mag_size * 0.6;
+  })
+  .attr('opacity', d => 0.8)
+  .attr('cursor', 'pointer')
+  .attr("cx", function(d, i) {
+    return  60   * d.term_level
+    })
+  .attr("cy", function(d, i) {
+     return mag_size * 7  * i;
+    //return 60 * d.term_level;
+    //return y(d.pfcLevel)
+  })
+  .attr("r",  mag_size * 2)
+  .append('title')
+      .text((d,i) => {
+        return ' Title:  ' 
+        + d.term_title 
+        + " Code: " 
+        + d.term_code
+        + " "
+        + " Level " 
+        + d.term_level
+           })
+
+    var link = groups.selectAll("circle")
+           .data( nodes.descendants().slice(1))
+         .enter().append("path")
+           .attr("class", "link")
+           .attr("d", function(d) {
+             console.log( "PARENT " +  d.parent)
+              return "M" + d.x + "," + d.y
+                + "L" + d.parent.x + "," + d.parent.y;
+              })
+              .style("fill", "none")
+              .style("stroke", "darkslateblue")
+              .style("stroke-width", "4px"); 
+   /*    
+  let  link = d3.linkHorizontal()
+           .x(function(d) {
+             return d.x;
+           })
+           .y(function(d) {
+             return d.y;
+           });
+ 
+           svg.append("path")
+           .attr("d", link(nodes.descendants()))
+           .style("fill", "none")
+           .style("stroke", "darkslateblue")
+           .style("stroke-width", "4px");
+*/
+    /*       
+    let link = svg.selectAll(".link")
+           .data( nodes.descendants().slice(1))
+         .enter().append("path")
+           .attr("class", "link")
+           .attr("d", function(d) {
+              return "M" + d.x + "," + d.y
+                + "C" + d.x + "," + (d.y + d.parent.y) / 2
+                + " " + d.parent.x + "," + (d.y + d.parent.y) 
+                + " " + d.parent.x + "," + d.parent.y;
+              })
+        .attr('transform', `translate(${width*0.06}, ${tree_origin_y-250})`);
+  */
+
+    
+    
+           let text = groups.selectAll("circle") // start a nested selection
+           .data( (d, i) => {
+             return d.values; // tell d3 where the children are
+           })
+         .append('text')
+          .attr('dx', (d, i) => {
+             return  80   * d.term_level
+                })
+        .attr('dy',  (d, i) => {
+          return mag_size * 7  * i;
+          //return 40 * i;
+          
+        })
+        //.attr("y", function(d, i) { 
+        //  console.log("CHILDREN" + JSON.stringify(d[i]) )
+        //  return d.children ? -20 : 20; })             
+        .attr('fill', '#fff')
+        .attr('text-anchor', 'start')
+        .style("font-family", "sans, georgia, times")
+        .style('font-size', '1.2em')
+        .style('font-weight', 'normal')
+        .style('opacity', '0.9')
+        .style('stroke', 'none')
+        .text( (d, i) => {
+          console.log("Titles: " + d.term_level  + " " + " " + d.term_title)      
+          return  d.term_title
+        }
+      
+        )
+     // .attr('transform', `translate(${width*0.06}, ${tree_origin_y})`);
+  
+ //////////////////////////////
+/*
+ let path = circles.append('svg:path')
+      .attr("d", digits_text_path )
+      .attr("id","digits_path")
+      .style("fill","none")
+      .style("stroke","none");
+
+ 
+circles.append("text")
+  .style("font-size", "1.0em")
+  .style("font-family","sans")
+  .style("fill","white")
+  //.style("fill",  function(d){return fill(d.axis);})
+  .style("opacity",1)
+  .append("textPath")
+  .attr("xlink:href", "#digits_path")
+  .text("HELLO JGOUR")
+  //.text(function(d, i ) { return d.values[i].term_title ;  });
+  */
+
+//////////////////////////  
+}
+///////////////////////
+diagram_2(group){
   let tree_origin_x = width * 0.01;
   let tree_origin_y = height * 0.1;
   
@@ -288,7 +691,7 @@ let treemap = d3.tree()
 .size([width, height]);
 
 
-  
+ 
 let  groups = svg.selectAll(".circle")
   .data(nested_data)
   .enter().append("g")
@@ -314,45 +717,42 @@ let  groups = svg.selectAll(".circle")
   .attr('opacity', d => 0.8)
   .attr('cursor', 'pointer')
   .attr("cx", function(d, i) {
-    return  12 * i ;
+   return  20 * d.term_level
+    //return  18 * i ;
    //return x(d.sampleDate) // use the fields directly; no reference to "values"
   })
   .attr("cy", function(d, i) {
-    return 60 * d.term_level;
+     return 50 * i;
+
+    // return 60 * d.term_level;
     //return y(d.pfcLevel)
   })
-  .attr("r",  mag_size * 2, 5)
+  .attr("r",  mag_size * 3, 5)
   .append('title')
       .text((d,i) => {
-        //console.log("DATA JGOUR " + JSON.stringify( d.data.term_title) )
-        return ' Term Title:  ' + d.data.term_title + " " + " Term Level " + d.term_level;
+        return ' Title:  ' 
+        + d.data.term_title 
+        + " Code: " 
+        + d.data.term_code
+        + " "
+        + " Term Level " 
+        + d.term_level;
       });
 
 //////////////////////////  
+
+//////////////////////////////
 };//diagram
 
 
 };//class Draw
   //////////////////////////
 
-  /*********************
-   * let terms = {"term_use":element["terms_use"],
-"term_title":element["terms_title"],
-"term_see_tab":element["term_see_tab"],
-"term_see_codes":element["term_see_codes"],
-"term_see":element["term_see"],
-"term_nemod":element["term_nemod"],
-"term_level":element["term_level"],
-"term_codes":element["term_codes"],
-"term_code":element["term_code"]
-};//terms
-
-  *********************/
+ 
 class Data_graph {
   set_graph(){
     
     let data = get_data();
-   //     console.log(JSON.stringify(data) );
 
     let g = new Graph({
        multi:false,
@@ -364,7 +764,6 @@ for (let i = 0; i < data.length; i++) {
         let n_i = data[i];
 
 
-        //console.log(JSON.stringify(n_i));
         /////////////////////////
         let a_id = i;
         let a_term_use = n_i['term_use'];
@@ -410,21 +809,61 @@ build_nodes(graph){
   graph.forEachNode((node, attributes) => {
     let template_i =    this.insert_to_template(node,  attributes );
    })//graph1 
+   
 
  nested_data = d3.nest()
    .key(function(d) { return d.term_level; }).sortKeys(d3.ascending)
    .entries(injury_template);
-
+   
   };//build_nodes
 /////////////////////
 
+get_parent(term_level){
+  let parent = "";
+  for(let i = 0; i < parent_child_template.length; i++ ){
+    let template_i = parent_child_template[i];
+    if (template_i["term_level"] == term_level) {
+        this.parent = template_i["parent"]; 
+      
+    };//if
+    };//for
+
+    return parent;
+
+}
+/////////////////////////////
+get_children(term_level){
+  let children = "";
+  for(let i = 0; i < parent_child_template.length; i++ ){
+    let template_i = parent_child_template[i];
+    if (template_i["term_level"] == term_level) {
+        this.children = template_i["children"]; 
+      
+    };//if
+    };//for
+
+    return parent;
+
+}/////
+
 insert_to_template(node, attributes){
-  let level = attributes["term_level"];
-  //console.log("term level ---jgour" + level )
+  //let level = attributes["term_level"];
+  let parent = this.get_parent(attributes["term_level"])
+  let children = this.get_children(attributes["term_level"])
+
   let an_item =  {
-    term_level: level,
-    data:   attributes,
-    parent:  (Number(level) -  Number(1)).toString(),
+    term_level:     attributes["term_level"],
+    term_code:      attributes["term_code"],
+    term_title:     attributes["term_title"],
+    term_see_tab:   attributes["term_see_tab"],
+    term_see_codes: attributes["term_see_codes"],
+    term_see:       attributes["term_see"],
+    term_nemod:     attributes["term_nemod"],
+    term_level:     attributes["term_level"],
+    term_codes:     attributes["term_codes"],
+    term_code:      attributes["term_code"],
+    parent:         parent,
+    children:       children
 
 };//an_item 
 
@@ -434,7 +873,6 @@ injury_template.push( an_item);
  
 };//insert_to_template
 
-/**************************/
 
 
 build_edges(graph) {
@@ -504,10 +942,6 @@ class Prep_data{
 start() {
   let graph = this.g.set_graph();
 //  console.log("NODES DATA: "+JSON.stringify(nodes_data) + " length " + nodes_data.length)
- // console.log("NESTED  DATA: "+JSON.stringify(nested_data) + " length " + nested_data.length)
- // console.log("INJURY TEMPLATE : "+JSON.stringify(injury_template) + " length " +  injury_template.length)
-
-  //console.log("NODES DATA: "+JSON.stringify(nodes_data) + "length " + nodes_data.length)
   /*
   console.log("***********************************");
   console.log("Graph type: " +  graph.type)
@@ -545,10 +979,6 @@ start() {
 
 }
 
-//console.log("======start level 1=======================")
-//a_level_1.push(["children", level_2])
-//console.log(JSON.stringify(a_level_1) )
-//console.log("======end level 1=======================")
 
 
 
@@ -752,7 +1182,6 @@ children: []}),
 };//class
 /////////////////////
 const draw_external_init = () => {
-//console.log("from draw_coronary_init ------------------------");
   
 const main = new Interface_buttons();
   
