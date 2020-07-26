49a628f7e0fa7ec5f1f99ae461df4f5d872a298d
jgourassas1948 <jgourassas@gmail.com>
2020-07-26 17:29:59 +0300

    Graphical icd10 Clinical added

--- assets/js/icd10cm_tree/icd10cm_tree.js
+++ assets/js/icd10cm_tree/icd10cm_tree.js
@@ -15,7 +15,6 @@ let margin = {
 let width = 1200 - margin.left - margin.right;
 let height = 1800 - margin.top - margin.bottom;
 let barHeight = 24;
-//let barWidth = width * .8;
 let barWidth = (width - margin.left - margin.right) * 0.8;
 
 let icd10cm_duration = 500;
@@ -24,16 +23,7 @@ var icd10cm_json;
 var root_icd10cm;
 var vis_icd10cm;
 
-let connector = (d) =>  {
-  //curved 
-  /*return "M" + d.y + "," + d.x +
-     "C" + (d.y + d.parent.y) / 2 + "," + d.x +
-     " " + (d.y + d.parent.y) / 2 + "," + d.parent.x +
-     " " + d.parent.y + "," + d.parent.x;*/
-   //straight
-   return "M" + d.parent.y + "," + d.parent.x
-     + "V" + d.x + "H" + d.y;      
- }
+
 var  treemap = d3.tree()
   .size([height, 160])
   //.nodeSize([0, 28]);
@@ -44,24 +34,32 @@ var  treemap = d3.tree()
     root.dy = width / (root.height + 1);
     return d3.tree().nodeSize([root.dx, root.dy])(root);
   }
+
   let diagonal = d3.linkHorizontal()
   .x(function (d) { return d.y; })
   .y(function (d) { return d.x; });
 
+//let colors = d3.scaleOrdinal(d3.schemeBlues[9]);
+//let colors  = d3.scaleOrdinal(d3.schemeBrBG[4]);
+//let colors =  d3.scaleOrdinal(d3.schemePastel1); 
+//let  colors = d3.scaleOrdinal(d3.schemeCategory10)
+// let colors = d3.scaleOrdinal(d3.schemeSet2)
+// let colors = d3.scaleOrdinal(d3.schemePiYG[7])
 
-
-const icd10cm_desc_color = (d) => {
-  return d._children ? "#87AAAE" : d.children ? "#BFD9DA" : "#DDECEF";
-
+ const icd10cm_desc_color = (d) => {
+  // return d._children ? "#87AAAE" : d.children ? "#BFD9DA" : "#DDECEF";
+   //return d._children ? colors(d._children): d.children ? colors(d.children) : colors(d._children);
+    return d._children ? "#87bdd8" : d.children ? "#b7d7e8" : "#cfe0e8";
 }
 
 const icd10cm_code_color = (d) => {
-  return d._children ? "#87AAAE" : d.children ? "#BFD9DA" : "#F6F6F6";
+  //return d._children ? "#87AAAE" : d.children ? "#BFD9DA" : "#F6F6F6";
+  return d._children ? "#87bdd8" : d.children ? "#b7d7e8" : "#cfe0e8";
 
 }
 const icd10cm_code_mark_color = (d) => {
   return d._children ? "#87AAAE" : d.children ? "#BFD9DA" : "#FF1CAE";
-  //return d._children ? "#87AAAE" : d.children ? "#BFD9DA" : "green";
+
 
 }
 const icd10cm_code_mark_stroke_color = (d) => {
@@ -69,21 +67,9 @@ const icd10cm_code_mark_stroke_color = (d) => {
 }
 
 
-let blues = d3.scaleOrdinal(d3.schemeBlues[9]);
-let colors = d3.schemeCategory10
 
 const mag_size = 1.9;
-/*
-let collapse = (d) => {
 
-  if (d.children) {
-    d._children = d.children;
-    d._children.forEach(collapse);
-    d.children = null;
-  }
-
-}//collapse
-*/
 ////////////////////////////
 let collapse = (d) =>  {
   d.Selected = false;
@@ -123,8 +109,20 @@ let get_data = (vis) => {
  let json_data_hierarchy = d3.hierarchy(json_data, function (d) {
       return d.children
     });
+   
+/////////////////////    
+/*
+let start_x = 50;
+let start_y = 100;
+    vis
+    .append('g')  
+    .attr("transform", "translate(" + start_x + "," + start_y + ")");
+  vis_icd10cm = vis;
+*/
+  ////////////////////////////
     vis_icd10cm = vis;
-
+   
+   
     icd10cm_json = json_data_hierarchy;
     json_data = null;
     
@@ -135,20 +133,7 @@ let get_data = (vis) => {
   
     let draw = new Draw()
     draw.test(root_icd10cm = icd10cm_json)
-
-    /*
-    let nodes = d3.hierarchy(json_data, function (d) {
-      return d.children
-    });
-   
-   nodes.x0 = 0;
-    nodes.y0 = 0;
-    nodes.children.forEach(collapse);
-
-    let draw = new Draw()
-    draw.test(nodes)
-
-*/
+ 
   });//requst
 
 
@@ -195,6 +180,7 @@ class Set_sketch {
   vis() {
     let svg_vis = this.svg();
     let vis = svg_vis.append('svg:g').attr('id', 'svg_vis');
+
     return vis;
   }
 } //class Set_sketch
@@ -214,43 +200,53 @@ class Draw {
     let tree_origin_x = width * 0.03;
     let tree_origin_y = height * 0.09;
 
-
-  /*  
-  let svg_dot = d3.select("#svg_vis").append("svg:svg")
+    
+  let svg_dot = vis_icd10cm
       .attr("width", width)
       .attr("height", height)
       .append("svg:g")
       .attr("id", "group_svg")
       .attr("transform", "translate(" + tree_origin_x + "," + tree_origin_y + ")");
     // declares a tree layout and assigns the size
-    */
    
-    d3.select("#group_svg").transition()
+   
+   d3.select("#group_svg").transition()
     .duration(icd10cm_duration)
     .attr("height", height);
 
+
+
     let nodes = treemap(root_icd10cm);
-  
+ 
   
     let index = -1;
     nodes.eachBefore(function(n) {
       n.x = ++index * barHeight ;
-      //n.y = n.y * 28;
-    });
+      });
   
 
+    let  node = vis_icd10cm.selectAll(".node")
+      .data(nodes.descendants(), function(d, i) { return d.id || (d.id = ++i); })
+      //.attr("transform", "translate(" + tree_origin_x + "," + tree_origin_y + ")");
 
-    var node = vis_icd10cm.selectAll(".node")
-    .data(nodes.descendants(), function(d, i) { return d.id || (d.id = ++i); })
-    .attr("transform", "translate(" + tree_origin_x + "," + tree_origin_y + ")");
-    //let node = svg_dot.selectAll(".node")
-    // .data(nodes.descendants(), function (d, i) { return d.id || (d.id = ++i); })
 
       let nodeEnter = node.enter().append('svg:g')
       .attr("class", "node")
-      .attr("transform", function(source, i) { 
-       return "translate(" + source.y0 + "," + source.x0 + ")"; })
-  
+      .on("click", (d, i) => {
+        if (d.children) {
+          d._children = d.children;
+          d.children = null;
+        } else {
+          d.children = d._children;
+          d._children = null;
+        }
+       this.test(d);
+      })
+      //.attr("transform", function(source, i) { 
+      //  console.log("source.y0 " +  source.y0)
+     //  return "translate(" + source.y0  + "," + source.x0 + ")"; })
+       
+/*
       nodeEnter.append('circle')
       .attr('class', 'circle')
       .attr('r', 10)
@@ -260,7 +256,7 @@ class Draw {
           return d._children ? "yellow" : "red";
       } );
 
-
+*/
     nodeEnter.append("rect")
       .attr("id", "first_rect")
       .attr("y", -barHeight / 2)
@@ -271,11 +267,9 @@ class Draw {
       .style("stroke", "#FFFCE5")
       .attr("stroke-width", 2)
       .style("opacity", 0.9)
-      //.style("fill", function(d) { return d._children ? "lightsteelblue" : "#F012BE"; })
       .style("fill", icd10cm_desc_color)
+     /*
       .on("click", (d, i) => {
-      //   d.children = d.children ? null : d._children
-      
         if (d.children) {
           d._children = d.children;
           d.children = null;
@@ -283,11 +277,9 @@ class Draw {
           d.children = d._children;
           d._children = null;
         }
-       
-        
        this.test(d);
       })
-
+*/
     nodeEnter.append("text")
       .attr("dy", 4.5)
       .attr("dx", 5.5)
@@ -324,13 +316,24 @@ class Draw {
       // .on("click", icd10cm_click)
       .text(function (d) { return (!d.data.code) ? null : d.data.code; });
 
+      nodeEnter.append("svg:rect")
+      .attr("y", -barHeight / 6)
+      .attr("height", barHeight/4)
+      .attr("x",      barWidth-20.0)
+      .attr("width",  barHeight/4)
+      .attr("rx", 2)
+      .attr("ry", 4)
+      .style("stroke",icd10cm_code_mark_stroke_color)
+      .attr("stroke-width", 0.2)
+      .style("fill", icd10cm_code_mark_color);
+
 
     let symbol = d3.symbol().type(d3.symbolCircle).size(100);
 
     let a_symbol = d3.selectAll(".node")
       .append("path")
       .attr("d", symbol)
-      .style("fill", d => d.children ? "#ffffb3" : "#8dd3c7")
+      .style("fill", icd10cm_code_mark_color)
       .style("cursor", "pointer")
       .attr("pointer-events", "all")
     
@@ -345,33 +348,18 @@ class Draw {
       .clone(true).lower()
       .attr("stroke", "white");
 ////////TRANSITIONS///////////////////////
-
-
-nodeEnter.append("rect")
-.attr("y", -barHeight / 2)
-.attr("height", barHeight )
-.attr("x", barWidth - 110.0)
-.attr("width", 80)
-.attr("rx", 2)
-.attr("ry", 2)
-.style("stroke", "red")
-.attr("stroke-width", 2)
-.style("fill", "none");
-
 this.utils.node_enter_move(nodeEnter, nodes.descendants())
 this.utils.node_move(node, nodeEnter, nodes.descendants());
 this.utils.node_exit(node, source)
-//let nodeExit = d3.select( (d, i) => {return "#node" + i})
-//.transition()
 
 
   
 
 
 ///////////////////
-/*
+
 // Update the links…
-var link = svg_dot.selectAll("path.link")
+var link = vis_icd10cm.selectAll("path.link")
 .data(nodes.links(), function(d) { return d.target.id; });
 
 // Enter any new links at the parent's previous position.
@@ -383,7 +371,9 @@ link.enter().insert("path", "g")
   })
   .style("fill", "none")
   .style("stroke", "#9ecae1")
-  .style("stoke-width", 1.5)
+  //.style("stroke",icd10cm_code_mark_stroke_color)
+  //.style("stroke", icd10cm_code_mark_color)
+  .style("stoke-width", 1.0)
 .transition()
   .duration(icd10cm_duration)
   .attr("d", diagonal);
@@ -402,17 +392,12 @@ link.exit().transition()
   })
   .remove();
 /////////////////
-*/
+
 
 nodes.each(function(d) {
      d.x0 = d.x;
      d.y0 = d.y;
    });
-
-
-
-
-
 
     ///////////
   };//test
@@ -463,12 +448,10 @@ export class Tree_buttons {
             let setup = new Set_sketch();
             ////////////////////////////////
             let vis = setup.vis();
-            let guides = new Guides();
-            let bullets_group = guides.make_bullets(vis);
+            //let guides = new Guides();
+            //let bullets_group = guides.make_bullets(vis);
             let draw = new Draw()
-            //draw.icd10cm(vis);
-            //get_data();
-            get_data(vis)
+             get_data(vis)
 
             //draw.prototype(vis);
           }
@@ -576,51 +559,28 @@ class Utils {
  node_enter_move(nodeEnter, d){ 
   nodeEnter.transition()
   .duration(icd10cm_duration)
+  .ease(d3.easeLinear)
   .attr("transform", function(d) { 
-    console.log("1111111111111111111111111111")
-   //console.log("d.x: " +d.x)
-   //console.log("d.y: " +d.y)
-    return "translate(" + d.y + "," + d.x + ")"; })
+     return "translate(" + d.y + "," + d.x + ")"; })
   .style("opacity", 1);
   }
 
 node_move(node, nodeEnter, d) {
-  console.log("22222222 node move 22222222222222222222222222")
-/*
-  node.merge(nodeEnter).transition(icd10cm_duration)
-  .attr("transform", d => `translate(${d.y},${d.x})`)
-  .attr("fill-opacity", 1)
-  .attr("stroke-opacity", 1);
-*/
-  
   
   node.transition()
   .duration(icd10cm_duration)
+  .ease(d3.easeLinear)
   .attr("transform", (d) =>  { 
    return "translate(" + d.y + "," + d.x + ")"; })
     }
 
 node_exit(node, source){
-  console.log("33333333333 node exit 333333333333333333333")
   //Transition exiting nodes to the parent's new position.
    node.exit().transition(icd10cm_duration).remove()
        .attr("transform", d => `translate(${source.y},${source.x})`)
-    //.attr("transform", d => `translate(${d.y},${d.x})`)
-    .attr("fill-opacity", 0)
-    .attr("stroke-opacity", 0);
+       .attr("fill-opacity", 1)
+       .attr("stroke-opacity", 1);
   
-  /*
-  console.log("3333333333333")
-  console.log("source.y ---------------------- "+ JSON.stringify( source.y));
-
-   node.exit().transition()
-  .duration(icd10cm_duration)
-  .attr("transform", function(source) { 
-  // console.log("source.y ---------------------- "+ source.y)
-     return "translate(" + source.y + "," + source.x + ")"; })
-  .style("opacity", 1)
-  .remove();
-*/
 
 }
 
@@ -653,7 +613,7 @@ node_exit(node, source){
 
   icd10cm_normal_fonts(d, i) {
     var fonts = d3.select(this);
-    fonts.style("font-size", "0.85em");
+    fonts.style("font-size", "0.95em");
   }
 
   icd10cm_highlite(d) {
@@ -675,6 +635,13 @@ node_exit(node, source){
 
     return a_title;
   }
+
+  show_rain_drop(size) {
+    let  r = Math.sqrt(size / Math.PI) 
+    let drop = "M" + r + ",0" + "A" + r + "," + r  + " 0 1,1 " + -r + ",0" + "C" + -r + "," + -r  + " 0," + -r + " 0," + -3*r + "C0," + -r + " "  + r + "," + -r + " " + r + ",0" + "Z";
+    return drop
+
+   };//rain_drop
 
 };//Utils
 
