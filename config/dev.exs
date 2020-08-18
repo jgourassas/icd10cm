2eb29df7802cd306be346cacccaba036850e38bf
jgourassas1948 <jgourassas@gmail.com>
2020-08-18 10:44:33 +0300

    Graphical ICD-10-CM added

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ config :icd10cm, Icd10cm.Repo,
   database: "icd10cm_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 15
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
@@ -67,10 +67,10 @@ config :icd10cm, Icd10cmWeb.Endpoint,
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
-
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
+config :elixir, ansi_enabled: true
 
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
